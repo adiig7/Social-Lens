cdb42cabba7e3311ad1f0b0fb32fecf886896cb4
Aditya Gupta <adityagupta765469@gmail.com>
2022-08-21 17:37:17 +0530

    dapp complete

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import { useQuery } from "@apollo/client";
+import recommendedProfiles from '../queries/recommendedProfilesQuery'
+import Profile from '../components/Profile.js';
+
+export default function Home() {
+  const {loading, error, data} = useQuery(recommendedProfiles);
+
+  if (loading) return 'Loading..';
+  if (error) return `Error! ${error.message}`;
+
+  return (
+    <div>
+      {data.recommendedProfiles.map((profile, index) => {
+        console.log(`Profile ${index}:`, profile);
+        return <Profile key={profile.id} profile={profile} displayFullProfile={false} />;
+      })}
+    </div>
+  )
+}
